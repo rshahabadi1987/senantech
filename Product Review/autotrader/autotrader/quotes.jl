d1aadd0d162517d0f6dfb0c3d331fcaf9f9203aa
rshahabadi1987 <rshahabadi1987@gmail.com>
2017-07-13 13:10:11 +0530

    Add files via upload
    Product review , auto traders scrapy code.

---
+++ Product Review/autotrader/autotrader/quotes.jl
@@ -0,0 +1,24 @@
+{"text": ["Affordable to buy", "Exemplary reliability record", "Good off-road"]}
+{"text": ["Abysmal on-road", "Poor quality", "Not very practical"]}
+{"text": ["Funky styling", "Cheap to buy and economical to run", "Tardis-like interior space"]}
+{"text": ["Ride and refinement aren\u2019t great", "Interior trim looks and feels cheap", "Safety kit is trim dependent"]}
+{"text": ["Cabin is reasonably roomy and practical", "The best models are cheap to buy and run", "Petrol engines are smooth and quiet "]}
+{"text": ["Diesel engine is rough and extremely noisy ", "Cabin sounds open to the elements", "Some interior panels look and feel rather cheap"]}
+{"text": ["Cabin has lots of passenger space", "Long list of standard equipment", "Refined and strong three-cylinder engine"]}
+{"text": ["Some below-par materials in cabin", "Ride and handling not as sharp as some rivals\u2019", "Some desirable safety kit only on top-spec models"]}
+{"text": ["Huge interior space for a small car", "Affordable prices and generous standard kit", "Enviable reliability record"]}
+{"text": ["Cheap-feeling interior", "Not as good as many rivals to drive", "Disappointing Euro NCAP result"]}
+{"text": ["Good fun to drive", "Cracking turbocharged engine", "Should be affordable to buy and run"]}
+{"text": ["Interior feels a little low-rent", "Only range-topping version has clever safety kit", "Boot could be bigger"]}
+{"text": ["It looks like a saloon version of the Swift Sport", "The handling is good enough to back up its stocky looks", "It\u2019s impressively kitted out for the price"]}
+{"text": ["The big petrol engine is too thirsty and expensive to run", "Its central displays and gauges look a bit dated", "Rarity could make maintenance and repairs more expensive in the years to come"]}
+{"text": ["Cabin is roomy and practical", "Pretty cheap to buy and run", "All versions are well equipped"]}
+{"text": ["Engines are noisy and gutless at low revs", "Ride is a little too unsettled", "Some interior panels feel rather cheap"]}
+{"text": ["Significantly improved interior", "Enjoyable to drive", "Improved petrol engine offers more power"]}
+{"text": ["Limited luggage capacity", "Pillars restricted visibility", "Steering wheel adjusts for reach only"]}
+{"text": ["Great fun to drive", "Funky styling", "Engines are pretty economical"]}
+{"text": ["Refinement isn\u2019t great", "Automatic gearbox is best left alone", "Boot is small and awkward to access"]}
+{"text": ["Cheap", "Capable off-road", "Reliable"]}
+{"text": ["Very slow", "Cramped interior", "Poor dynamically"]}
+{"text": ["Smart looks and lots of styling options", "Very generous with standard equipment", "Suzuki has an enviable reliability record"]}
+{"text": ["Pretty mediocre to drive", "Interior plastics feel cheap and flimsy", "Some questionable ergonomics"]}
